a4878dd89e3030fde74bd8bc63bf819a5af96d02
v-bdt <valentinbauduret@yahoo.fr>
2026-04-04 20:59:34 +0200

    Update enumLIN.sh

--- enumLIN.sh
+++ enumLIN.sh
@@ -39,23 +39,6 @@ function user_checks { # Checks current user
 	echo -e "\nConnected users : \n$CONNECTED_USERS"
 
 	echo -e "\nHome repositories : \n$HOME\n"
-
-
-# Checks writable files for user
-
-	echo -e "$CYAN\n---CHECKING WRITABLE FILES FOR CURRENT USER AND ITS GROUPS...---\n$WHITE"
-
-	current_user=$(whoami)
-	echo -e "Writable files for $current_user :\n"
-	find / -perm -u+w -user $current_user 2>/dev/null | grep -vE "proc|run|sys"
-	find / -perm -o+w 2>/dev/null | grep -vE "proc|run|sys"
-
-#GROUPS=($(id -nG))
-#for group in $GROUPS
-#do
-#	echo -e "Writable files for "$group"\n\n"
-#	find / -perm -g+w -group $group 2>/dev/null | grep -vE "proc|run|sys"
-#done
 }
 
 function fs_checks {
@@ -88,7 +71,68 @@ function files_checks {
 	else
 		echo -e "$RED\nHistory files found :\n\n $HISTORY"
 	fi
+	
+	# Checks writable files for user
+
+	echo -e "$CYAN\n---CHECKING WRITABLE FILES FOR CURRENT USER AND ITS GROUPS...---\n$WHITE"
+
+	current_user=$(whoami)
+	echo -e "Writable files for $current_user :\n"
+	find / -perm -u+w -user $current_user 2>/dev/null | grep -vE "proc|run|sys"
+	find / -perm -o+w 2>/dev/null | grep -vE "proc|run|sys"
+
+	#GROUPS=($(id -nG))
+	#for group in $GROUPS
+	#do
+	#	echo -e "Writable files for "$group"\n\n"
+	#	find / -perm -g+w -group $group 2>/dev/null | grep -vE "proc|run|sys"
+	#done
+}
+
+function netconf_checks {
+
+	echo -e "$CYAN\n---CHECKING NETWORK CONFIGURATION...---\n$WHITE"
+	echo -e "\nSockets listening on 127.0.0.1 :\n"
+	ss -tulnp && netstat -tulnp | grep 127.0.0.1
+	
+	echo -e "\nConfigured DNS :\n"
+	cat /etc/resolv.conf
+	
+	echo -e "\nHosts file :\n"
+	cat /etc/hosts
+	
+	echo -e "\nARP Table :\n"
+	arp -a
+	
+	echo -e "\nRouting Table :\n"
+	netstat -rn
+}
+
+function process_checks {
+
+	echo -e "$CYAN\n---CHECKING RUNNING PROCESSES...---\n$WHITE"
+	ps -aux
+	echo -e "$ORANGE\nDon't forget to launch pspy too with ./pspy64 -pf -i 1000 | grep 'UID=0' !"
+}
+
+function os_infos {
+
+	echo -e "$CYAN\n---CHECKING OS INFOS...---\n$WHITE"
+	
+	echo -e "\nOS Version :\n"
+	cat /etc/os-release
+
+	echo -e "\nKernel Version :\n"
+	uname -a
+	cat /proc/version
+	
+	echo -e "$WHITE\nCPU infos :\n"
+	lscpu
 }
 
 user_checks
 fs_checks
+files_checks
+netconf_checks
+process_checks
+os_infos
